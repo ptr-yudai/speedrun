5d483f03d526273227fe4523ad4ce85774c696d2
theoremoon <theoldmoon0602@gmail.com>
2021-12-17 22:34:36 +0900

    add admin info

--- ui/src/view/Index.tsx
+++ ui/src/view/Index.tsx
@@ -1,6 +1,59 @@
+import kureanifURL from "../assets/kurenaif.png";
+import rekkusuURL from "../assets/rekkusu.png";
+import ptrYudaiURL from "../assets/ptr-yudai.jpeg";
+import keymoonURL from "../assets/keymoon.jpg";
+import moratorium08URL from "../assets/moratorium08.png";
+import theoremoonURL from "../assets/theoldmoon0602.png";
+
 const Index = () => {
   return (
     <>
+      <section>
+        <h1>Speedrun Challenge</h1>
+
+        <h2>出演</h2>
+        <div className="admin-flex">
+          <a href="https://twitter.com/xrekkusu" className="admin">
+              <img src={rekkusuURL} className="admin-icon" />
+              <p className="admin-description">司会・実況</p>
+          </a>
+
+          <a href="https://twitter.com/ptrYudai" className="admin">
+            <img src={ptrYudaiURL} className="admin-icon" />
+            <p className="admin-description">作問・実況</p>
+          </a>
+
+          <a href="https://twitter.com/fwarashi" className="admin">
+            <img src={kureanifURL} className="admin-icon" />
+            <p className="admin-description">crypto走者</p>
+          </a>
+
+          <a href="https://twitter.com/theoremoon" className="admin">
+            <img src={theoremoonURL} className="admin-icon" />
+            <p className="admin-description">crypto走者</p>
+          </a>
+
+          <a href="https://twitter.com/moratorium08" className="admin">
+            <img src={moratorium08URL} className="admin-icon" />
+            <p className="admin-description">pwn走者</p>
+          </a>
+
+          <a href="https://twitter.com/kymn_" className="admin">
+            <img src={keymoonURL} className="admin-icon" />
+            <p className="admin-description">pwn走者</p>
+          </a>
+        </div>
+      </section>
+
+      <section>
+        <h2>配信</h2>
+        <p>12月19日（日） 15:40(JST) 〜</p>
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/VXaROnAmAiY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+      </section>
+
+
       <section>
         <h2>Rule</h2>
         <ul>
